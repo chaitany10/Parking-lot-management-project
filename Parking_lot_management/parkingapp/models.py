05391b2c0908a3b4370aca1d94abfe171d890b4d
sscode1 <siddharth12375@gmail.com>
2019-10-22 21:13:57 +0530

    3nf to tables

--- Parking_lot_management/parkingapp/models.py
+++ Parking_lot_management/parkingapp/models.py
@@ -18,24 +18,29 @@ class block(models.Model):
     is_block_full=models.BooleanField(default=False)
     no_of_floors=models.IntegerField(default=1)
     block_code=models.CharField(max_length=30)
-
+    class Meta:
+        unique_together = (('parking_lot_id', 'block_id'),)
 
 class floor(models.Model):
     block_id = models.ForeignKey(block, on_delete=models.CASCADE)
-    floor_id = models.AutoField(unique=True, primary_key=True)
+    floor_id = models.AutoField(unique=True,primary_key=True)
     is_floor_full = models.BooleanField(default=False)
     is_covered = models.BooleanField(default=False)
-    floor_number = models.AutoField(unique=True, primary_key=True)
+    floor_number = models.IntegerField(unique=True)
     max_height = models.IntegerField()
     no_of_slots = models.IntegerField(default=1)
     no_of_wings = models.IntegerField(default=1)
     block_code = models.CharField(max_length=30)
+    class Meta:
+        unique_together = (('block_id', 'floor_id'),)
 
 class parking_slot(models.Model):
     floor_id=models.ForeignKey(floor,on_delete=models.CASCADE)
     parking_slot_id=models.AutoField(unique=True,primary_key=True)
     wing_code=models.CharField(max_length=10)
     slot_no=models.IntegerField(default=1)
+    class Meta:
+        unique_together = (('floor_id', 'parking_slot_id'),)
 
 
 
