fa1f39e667db8c9e7e62214748e09d37c2f6c2b8
chaitany10 <chaitany.pandiya@gmail.com>
2019-10-22 15:33:00 +0530

    Added model for accounts

--- Parking_lot_management/accounts/models.py
+++ Parking_lot_management/accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    GENDER = (
+        ('M', "Male"),
+        ('F', "Female"),
+    )
+    firstname = models.CharField(blank=True, max_length=50)
+    lastname = models.CharField(blank=True, max_length=50)
+    sex = models.CharField(blank=True, max_length=1, choices=GENDER)
+    phone = models.CharField(blank=True, max_length=10)
+
+    def get_populated_fields(self):
+        """
+        To collect form data
+        :return:
+        """
+        fields = {}
+        if self.firstname is not None:
+            fields['firstname'] = self.firstname
+        if self.lastname is not None:
+            fields['lastname'] = self.lastname
+        if self.sex is not None:
+            fields['sex'] = self.sex
+        if self.phone is not None:
+            fields['phone'] = self.phone
+
+        return fields
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
