47b5183e977e8adc6fafa3d1bab15f2d91c46518
chaitany10 <chaitany.pandiya@gmail.com>
2019-10-24 22:22:41 +0530

    Added Accounts admin

--- Parking_lot_management/accounts/admin.py
+++ Parking_lot_management/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Customer, Regular_Customer, Vehicle_Numbers, Cost
+
+
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'firstname', 'lastname', 'sex', 'phone')
+
+
+@admin.register(Regular_Customer)
+class Regular_CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_id', 'purchase_date', 'start_date', 'pass_cost')
+
+
+@admin.register(Vehicle_Numbers)
+class Vehicle_Admin(admin.ModelAdmin):
+    list_display = ('customer_id', 'vehicle_no')
+
+@admin.register(Cost)
+class CostAdmin(admin.ModelAdmin):
+    list_display = ('cost', 'duration')
+
