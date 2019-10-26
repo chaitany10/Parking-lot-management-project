98eb45d81b8a36d6cfbab000e60d13e9c254f438
chaitany10 <chaitany.pandiya@gmail.com>
2019-10-26 19:33:27 +0530

    Home working

--- Parking_lot_management/Parking_lot_management/settings.py
+++ Parking_lot_management/Parking_lot_management/settings.py
@@ -123,6 +123,7 @@ LOGIN_URL = 'home'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT='staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static'),
 ]
