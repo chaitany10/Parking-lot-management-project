30f6a71c6d11a4a4137aa7cea755ebbe0b7ddc40
Chaitany Pandiya <32352441+chaitany10@users.noreply.github.com>
2019-10-26 19:45:49 +0530

    Update urls.py

--- Parking_lot_management/accounts/urls.py
+++ Parking_lot_management/accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('login', views.login_view, name='login'),
-    path('register', views.register, name='register'),
-    path('home', views.home, name='home'),
-    path('logout', views.logout, name='logout')
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout, name='logout')
 ]
