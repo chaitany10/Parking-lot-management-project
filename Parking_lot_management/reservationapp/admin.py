7f9de7ce1f4415fcdb41ff726e6226cea27594c8
chaitany10 <chaitany.pandiya@gmail.com>
2019-10-29 00:15:34 +0530

    Added models for parking_slip and reservation

--- Parking_lot_management/reservationapp/admin.py
+++ Parking_lot_management/reservationapp/admin.py
@@ -6,11 +6,9 @@ from .models import parking_slot_reservation, parking_slip
 # Register your models here.
 @admin.register(parking_slot_reservation)
 class parkingLotAdmin(admin.ModelAdmin):
-    list_display = ('id','customer_id','start_time_stamp','duration_in_minutes','booking_date','parking_slot_id')
+    list_display = ('id', 'customer_id', 'start_time_stamp', 'duration_in_minutes', 'booking_date', 'parking_slot_id')
 
 
 @admin.register(parking_slip)
 class blockAdmin(admin.ModelAdmin):
-    list_display = ('id','actual_entry_time','actual_exit_time','basic_cost','is_paid','parking_slot_id')
-
-
+    list_display = ('id', 'actual_entry_time', 'actual_exit_time', 'basic_cost', 'is_paid', 'parking_slot_id')
--- Parking_lot_management/reservationapp/views.py
+++ Parking_lot_management/reservationapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import parking_slot_reservation, parking_slip
 # Create your views here.
+from accounts.models import Customer
+from parkingapp.models import parking_slot
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+
+def reserve(request, parking_slot_id, duration):
+    if request.user.is_authenticated:
+        parking_slot_reservation = parking_slot()
+        user = request.user.username
+        parking_slot_reservation.customer_id =user
+        parking_slot_reservation.parking_slot_id = parking_slot_id
+        parking_slot_reservation.duration_in_minutes = duration
+        parking_slot_reservation.save()
+        return HttpResponseRedirect(reverse('parking_slip'), parking_slot_reservation)
+    else:
+        return HttpResponseRedirect(reverse('home'))
+
+def print_parking_slip(request, parking_slot_reservation):
+    bill = parking_slip()
