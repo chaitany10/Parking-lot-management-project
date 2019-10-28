78479f33396c3aeb167eb690a129e058f861d269
sscode1 <siddharth12375@gmail.com>
2019-10-28 21:56:19 +0530

    reservation model.py corrected

--- Parking_lot_management/reservationapp/models.py
+++ Parking_lot_management/reservationapp/models.py
@@ -1,20 +1,20 @@
-# from django.db import models
-# from .. accounts.models import Customer
-# from .. parkingapp.models import parking_slot
+from django.db import models
+from accounts.models import Customer
+from parkingapp.models import parking_slot
 
-# #Create your models here.
-# class parking_slot_reservation(models.Model):
-#     id=models.AutoField(primary_key=True)
-#     customer_id=models.ForeignKey(Customer,on_delete=models.CASCADE)
-#     start_time_stamp=models.DateTimeField(auto_now=True)
-#     duration_in_minutes=models.IntegerField()
-#     booking_date=models.DateField(auto_now=True)
-#     parking_slot_id=models.ForeignKey(parking_slot,on_delete=models.CASCADE)
-# class parking_slip(models.Model):
-#     id=models.AutoField(primary_key=True)
-#     actual_entry_time=models.DateTimeField(auto_now=True)
-#     actual_exit_time=models.DateTimeField()
-#     basic_cost=models.IntegerField()
-#     is_paid=models.BooleanField(default=True)
-#     parking_slot_id=models.ForeignKey(parking_slot,on_delete=models.CASCADE)
+#Create your models here.
+class parking_slot_reservation(models.Model):
+    id=models.AutoField(primary_key=True)
+    customer_id=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    start_time_stamp=models.DateTimeField(auto_now=True)
+    duration_in_minutes=models.IntegerField()
+    booking_date=models.DateField(auto_now=True)
+    parking_slot_id=models.ForeignKey(parking_slot,on_delete=models.CASCADE)
+class parking_slip(models.Model):
+    id=models.AutoField(primary_key=True)
+    actual_entry_time=models.DateTimeField(auto_now=True)
+    actual_exit_time=models.DateTimeField()
+    basic_cost=models.IntegerField()
+    is_paid=models.BooleanField(default=True)
+    parking_slot_id=models.ForeignKey(parking_slot,on_delete=models.CASCADE)
 
