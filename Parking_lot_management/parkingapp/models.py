5f2018ea2dfe392055991350215b70b8729bdcd1
chaitany10 <chaitany.pandiya@gmail.com>
2019-10-28 14:46:43 +0530

    Added gitignore

--- Parking_lot_management/parkingapp/models.py
+++ Parking_lot_management/parkingapp/models.py
@@ -1,33 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
 class parkingLot(models.Model):
-     parking_lot_id=models.AutoField(unique=True,primary_key=True)
-     is_slot_available=models.BooleanField(default=True)
-     is_reentry_allowed=models.BooleanField(default=False)
-     no_of_blocks=models.IntegerField(default=1)
-     pincode=models.IntegerField()
-     is_valet_available=models.BooleanField(default=False)
-     landmark=models.CharField(max_length=30)
-     building_no=models.IntegerField()
-     street_name=models.CharField(max_length=30)
-     def __str__(self):
-         return str(self.parking_lot_id)
+    parking_lot_id = models.AutoField(unique=True, primary_key=True)
+    is_slot_available = models.BooleanField(default=True)
+    is_reentry_allowed = models.BooleanField(default=False)
+    no_of_blocks = models.IntegerField(default=1)
+    pincode = models.IntegerField()
+    is_valet_available = models.BooleanField(default=False)
+    landmark = models.CharField(max_length=30)
+    building_no = models.IntegerField()
+    street_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.parking_lot_id)
+
 
 class block(models.Model):
-    parking_lot_id=models.ForeignKey(parkingLot,on_delete=models.CASCADE)
-    block_id=models.AutoField(unique=True,primary_key=True)
-    is_block_full=models.BooleanField(default=False)
-    no_of_floors=models.IntegerField(default=1)
-    block_code=models.CharField(max_length=30)
+    parking_lot_id = models.ForeignKey(parkingLot, on_delete=models.CASCADE)
+    block_id = models.AutoField(unique=True, primary_key=True)
+    is_block_full = models.BooleanField(default=False)
+    no_of_floors = models.IntegerField(default=1)
+    block_code = models.CharField(max_length=30)
+
     class Meta:
         unique_together = (('parking_lot_id', 'block_id'),)
+
     def __str__(self):
-         return str(self.block_id)
+        return str(self.block_id)
+
 
 class floor(models.Model):
     block_id = models.ForeignKey(block, on_delete=models.CASCADE)
-    floor_id = models.AutoField(unique=True,primary_key=True)
+    floor_id = models.AutoField(unique=True, primary_key=True)
     is_floor_full = models.BooleanField(default=False)
     is_covered = models.BooleanField(default=False)
     floor_number = models.IntegerField(unique=True)
@@ -35,17 +41,22 @@ class floor(models.Model):
     no_of_slots = models.IntegerField(default=1)
     no_of_wings = models.IntegerField(default=1)
     block_code = models.CharField(max_length=30)
+
     class Meta:
         unique_together = (('block_id', 'floor_id'),)
+
     def __str__(self):
-         return str(self.floor_id)
+        return str(self.floor_id)
+
 
 class parking_slot(models.Model):
-    floor_id=models.ForeignKey(floor,on_delete=models.CASCADE)
-    parking_slot_id=models.AutoField(unique=True,primary_key=True)
-    wing_code=models.CharField(max_length=10)
-    slot_no=models.IntegerField(default=1)
+    floor_id = models.ForeignKey(floor, on_delete=models.CASCADE)
+    parking_slot_id = models.AutoField(unique=True, primary_key=True)
+    wing_code = models.CharField(max_length=10)
+    slot_no = models.IntegerField(default=1)
+
     class Meta:
         unique_together = (('floor_id', 'parking_slot_id'),)
+
     def __str__(self):
-         return str(self.parking_slot_id)
+        return str(self.parking_slot_id)
