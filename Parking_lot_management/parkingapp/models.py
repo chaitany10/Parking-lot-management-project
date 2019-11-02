35035d1e86e2c9f386487424fe29b4aa6141825f
chaitany10 <chaitany.pandiya@gmail.com>
2019-11-02 14:57:15 +0530

    added cost and height

--- Parking_lot_management/parkingapp/models.py
+++ Parking_lot_management/parkingapp/models.py
@@ -13,6 +13,8 @@ class parkingLot(models.Model):
     landmark = models.CharField(max_length=30)
     building_no = models.IntegerField(validators=[MinValueValidator(1)])
     street_name = models.CharField(max_length=30)
+    cost = models.IntegerField(default= 10)
+    name = models.CharField(max_length=20, default='parkingLot)
     def __str__(self):
         return str(self.parking_lot_id)
 
@@ -51,6 +53,7 @@ class parking_slot(models.Model):
     wing_code = models.CharField(max_length=10)
     slot_no = models.IntegerField(default=1)
     is_reserved=models.BooleanField(default=False)
+    height = models.FloatField(default = 3)
     class Meta:
         unique_together = (('floor_id', 'parking_slot_id'),)
     def __str__(self):
